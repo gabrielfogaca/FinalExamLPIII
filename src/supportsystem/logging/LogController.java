fa1352259431c67aa3385614fe589b57d08aac82
Joao Ribas <joao.vmr@sou.unijui.edu.br>
2022-11-13 22:58:56 -0300

    Fixed: logging

--- src/supportsystem/logging/LogController.java
+++ src/supportsystem/logging/LogController.java
@@ -4,11 +4,13 @@
  */
 package supportsystem.logging;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.XMLFormatter;
 
@@ -17,49 +19,53 @@ import java.util.logging.XMLFormatter;
  * @author joao.vmr
  */
 public class LogController {
-    
+
+    static Properties conf = getLogConf();
+    static Handler handler;
     private static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(LogController.class.getPackage().getName());
 
-    public static void createLog (String mensagem, String nivel) {
-        try {
-            Properties conf = getLogConf();
+    private static Handler getHandler() {
 
-            Handler handler = new FileHandler(conf.getProperty("arquivo"), Boolean.parseBoolean(conf.getProperty("acrescentar")));
+        if (handler == null) {
+
+            try {
+                handler = new FileHandler(conf.getProperty("arquivo"), Boolean.parseBoolean(conf.getProperty("acrescentar")));
+            } catch (IOException ex) {
+                Logger.getLogger(LogController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SecurityException ex) {
+                Logger.getLogger(LogController.class.getName()).log(Level.SEVERE, null, ex);
+            }
             java.util.logging.Logger.getLogger("").addHandler(handler);
-
-            // define o formato de saída  
             if (conf.getProperty("formato-saida").equalsIgnoreCase("XML")) {
                 handler.setFormatter(new XMLFormatter());
             } else if (conf.getProperty("formato-saida").equalsIgnoreCase("SIMPLE")) {
                 handler.setFormatter(new SimpleFormatter());
             }
 
-            
+        }
 
-            if(nivel.equals("S")){
+        return handler;
+
+    }
+
+    public static void createLog(String mensagem, String nivel) {
+        getHandler();
+        if (nivel.equals("S")) {
             logger.severe(mensagem);
-            }
-            if(nivel.equals("W")){
+        }
+        if (nivel.equals("W")) {
             logger.warning(mensagem);
-            }
-            if(nivel.equals("I")){
+        }
+        if (nivel.equals("I")) {
             logger.info(mensagem);
-            }
-            
-            
-            
-
-        } catch (IOException | SecurityException ex) {
-            System.out.println(ex);
         }
 
     }
-   
+
 //    configurações do Log
     private static Properties getLogConf() {
         Properties conf = new Properties();
-        
-        
+
         conf.setProperty("arquivo", "SystemLog.log");
         conf.setProperty("acrescentar", "true");
         conf.setProperty("formato-saida", "SIMPLE");
@@ -67,5 +73,5 @@ public class LogController {
 
         return conf;
     }
-    
+
 }
